4ddf3338f7ed1f3a8d5f60994e07fdab74c69c08
Aniket Bhandari <166018090+Aniketbhandari18@users.noreply.github.com>
2025-09-25 07:49:49 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
 <?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 require_once __DIR__ . '/db.php';
 $stmt = $conn->prepare("SELECT id, title, body, created_at FROM notices WHERE is_active=1 ORDER BY created_at DESC");
 $stmt->execute();
